4583c79dd87ff977ea86bc14a161db410af600d5
juljaVysotska <julja.vysotska@gmail.com>
2018-06-24 14:43:28 +0300

    fix background in header

--- Easy-TV/views/site/index.php
+++ Easy-TV/views/site/index.php
@@ -13,12 +13,7 @@ use borales\extensions\phoneInput\PhoneInput;
 
 AppAsset::register($this);
 $this->title = 'Easy-TV';
-?><!--
-<link href="css/style.css">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>-->
+?>
 <?php
 /*try {
     $amo = Yii::$app->amocrm->getClient();
@@ -737,7 +732,7 @@ function addUser($name, $login, $email, $phone, $address)
                                         <p>
                                         <div class="row justify-content-center">
                                             <div class="col-md-12 col-sm-12">
-                                                <?= $form->field($model, 'phone')->textInput(['width' => '100%', 'class' => 'form-control'])->label(false)->widget(
+                                                <?= $form->field($model, 'phone')->textInput(['width' => '100%', 'class' => 'form-control','style' => 'min-width = 300%;'])->label(false)->widget(
                                                         PhoneInput::className(), [
                                                     'jsOptions' => [
                                                         'onlyCountries' => ['ru', 'ua'],
@@ -948,7 +943,7 @@ function addUser($name, $login, $email, $phone, $address)
             </script>
 <!--Head-->
 <div class="header"
-     style="height: 680px; background: rgba(15, 18, 49, 0.34) url('img/header/NEWSE.png') top center no-repeat; background-size: 100%">
+     style="height: 680px; background: rgba(15, 18, 49, 0.34) url('img/header/NEWSE.png') top center no-repeat; background-attachment: fixed; background-size: cover;">
     <div class="container">
         <div class="row">
             <div class="col-12">
